1ad05a95419addf77616863a118fe37c84553d56
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-06-10 17:59:24 +0200

    remove echo from deps

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.3
 require (
 	github.com/kazhuravlev/just v0.70.0
 	github.com/kazhuravlev/options-gen v0.33.0
-	github.com/labstack/echo/v4 v4.12.0
 	github.com/prometheus/client_golang v1.19.1
 	github.com/stretchr/testify v1.9.0
 )
@@ -21,7 +20,6 @@ require (
 	github.com/go-playground/validator/v10 v10.21.0 // indirect
 	github.com/goccy/go-yaml v1.11.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -29,8 +27,6 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.54.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
 	golang.org/x/net v0.26.0 // indirect
--- server/service.go
+++ server/service.go
@@ -2,9 +2,9 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/kazhuravlev/healthcheck"
-	"github.com/labstack/echo/v4"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log/slog"
 	"net/http"
@@ -27,13 +27,16 @@ func New(opts Options) (*Service, error) {
 }
 
 func (s *Service) Run(ctx context.Context) error {
-	echoInst := echo.New()
-	echoInst.HideBanner = true
-	echoInst.HidePort = true
+	mux := http.NewServeMux()
 
-	echoInst.GET("/live", s.handleLive)
-	echoInst.GET("/ready", s.handleReady)
-	echoInst.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+	mux.HandleFunc("/live", s.handleLive)
+	mux.HandleFunc("/ready", s.handleReady)
+	mux.Handle("/metrics", promhttp.Handler())
+
+	httpServer := &http.Server{
+		Addr:    ":" + strconv.Itoa(s.opts.port),
+		Handler: mux,
+	}
 
 	go func() {
 		<-ctx.Done()
@@ -41,40 +44,52 @@ func (s *Service) Run(ctx context.Context) error {
 		ctx, cancel := context.WithTimeout(context.WithoutCancel(ctx), 3*time.Second) //nolint:gomnd
 		defer cancel()
 
-		if err := echoInst.Shutdown(ctx); err != nil {
+		if err := httpServer.Shutdown(ctx); err != nil {
 			s.opts.logger.ErrorContext(ctx, "shutdown webserver", slog.String("error", err.Error()))
 		}
 	}()
 
 	go func() {
-		if err := echoInst.Start(":" + strconv.Itoa(s.opts.port)); err != nil {
-			s.opts.logger.ErrorContext(ctx, "start status server", slog.String("error", err.Error()))
+		if err := httpServer.ListenAndServe(); err != nil {
+			s.opts.logger.ErrorContext(ctx, "run status server", slog.String("error", err.Error()))
 		}
 	}()
 
 	return nil
 }
 
-func (s *Service) handleLive(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{})
+func (s *Service) handleLive(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 }
 
-func (s *Service) handleReady(c echo.Context) error {
-	report := s.opts.healthcheck.RunAllChecks(c.Request().Context())
-	switch report.Status {
-	case healthcheck.StatusUp:
-		return c.JSON(http.StatusOK, report)
-	case healthcheck.StatusDown:
-		s.opts.logger.ErrorContext(c.Request().Context(), "status error",
-			slog.Int("status", c.Response().Status),
-			slog.String("path", c.Path()),
-			slog.Any("report", report))
+func (s *Service) handleReady(w http.ResponseWriter, req *http.Request) {
+	const unknownResp = `{"status":"unknown","checks":[]}`
 
-		return c.JSON(http.StatusInternalServerError, report)
+	ctx := req.Context()
+	w.Header().Set("Content-Type", "application/json")
+
+	report := s.opts.healthcheck.RunAllChecks(ctx)
+	reportJson, err := json.Marshal(report)
+	if err != nil {
+		s.opts.logger.ErrorContext(ctx, "marshal report", slog.String("error", err.Error()))
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(unknownResp))
+		return
 	}
 
-	return c.JSON(http.StatusInternalServerError, map[string]any{
-		"status": "unknown",
-		"checks": []string{},
-	})
+	switch report.Status {
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(unknownResp))
+	case healthcheck.StatusUp:
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(reportJson)
+	case healthcheck.StatusDown:
+		s.opts.logger.ErrorContext(ctx, "status error", slog.Any("report", report))
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write(reportJson)
+	}
 }
