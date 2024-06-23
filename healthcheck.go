9a65a15a4fd16f899e7689acbc6c0d51e0a0bd08
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-06-23 23:48:01 +0200

    ka/gh-2 ka/gh-2 remove time from options

--- healthcheck.go
+++ healthcheck.go
@@ -1,7 +1,6 @@
 package healthcheck
 
 import (
-	"container/ring"
 	"log/slog"
 	"os"
 	"sync"
@@ -10,25 +9,22 @@ import (
 type Healthcheck struct {
 	opts hcOptions
 
-	checksMu    *sync.RWMutex
-	checks      []checkRec
-	checkStates map[string]*ring.Ring
+	checksMu *sync.RWMutex
+	checks   []checkContainer
 }
 
 func New(opts ...func(*hcOptions)) (*Healthcheck, error) {
 	options := hcOptions{
 		logger:         slog.New(slog.NewJSONHandler(os.Stdout, nil)),
 		setCheckStatus: func(string, Status) {},
-		time:           realTime{},
 	}
 	for _, opt := range opts {
 		opt(&options)
 	}
 
 	return &Healthcheck{
-		opts:        options,
-		checksMu:    new(sync.RWMutex),
-		checks:      nil,
-		checkStates: make(map[string]*ring.Ring),
+		opts:     options,
+		checksMu: new(sync.RWMutex),
+		checks:   nil,
 	}, nil
 }
