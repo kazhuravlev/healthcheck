1328e2106ef4d529a597cab5d2e3d05dc0869c45
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-06-24 09:43:21 +0200

    master add test cases

--- api_checks_test.go
+++ api_checks_test.go
@@ -3,6 +3,7 @@ package healthcheck
 import (
 	"context"
 	"github.com/stretchr/testify/require"
+	"io"
 	"testing"
 	"time"
 )
@@ -19,5 +20,26 @@ func TestManualCheck2(t *testing.T) {
 		require.Equal(t, time.Hour, check.timeout())
 		require.ErrorIs(t, check.check(context.TODO()).Error, errInitial)
 		require.Len(t, check.log(), 0)
+	})
+
+	t.Run("set_and_unset_error", func(t *testing.T) {
+		t.Parallel()
+
+		check := NewManual("sample")
+		require.Error(t, check.check(context.TODO()).Error)
+
+		check.SetErr(nil)
+		require.NoError(t, check.check(context.TODO()).Error)
+
+		check.SetErr(io.EOF)
+		require.ErrorIs(t, check.check(context.TODO()).Error, io.EOF)
+
+		t.Run("check_logs", func(t *testing.T) {
+			records := check.log()
+			require.Len(t, records, 2)
+
+			require.NoError(t, records[0].Error)
+			require.ErrorIs(t, records[1].Error, errInitial)
+		})
 	})
 }
